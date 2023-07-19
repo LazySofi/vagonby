2939f1a4356541d34420a747910ec9f982d93de0
s0fia_p <sofia.m.pisareva@gmail.com>
2023-07-19 17:05:44 +0300

    add more filters

--- home.py
+++ home.py
@@ -17,7 +17,7 @@ def drop_list_duplicates(k):
             new_k.append(elem)
     return new_k
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Поиск вагонов', 'Списки вагонов', '**Характристики**', 'Поиск по предприятиям', 'Список предприятий'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Поиск вагонов', 'Списки вагонов', '**Характеристики**', 'Поиск по предприятиям', 'Список предприятий'])
 
 # Поиск
 with tab1:
@@ -26,42 +26,47 @@ with tab1:
         model = st.text_input('Введите код модели', placeholder='Например: 11-5225')
     with col2:
         model_number = st.text_input('Введите номер модели', placeholder='Например: 2016')
+    col3, col4 = st.columns([3, 1])
+    with col3:
+        carrying = st.slider(
+            'Выберите границы грузоподьемности в тоннах',
+            0.0, 500.0, (0.0, 500.0))
+    with col4:
+        carrying_null = st.checkbox('Включать неизвестную грузоподьемность')
 
-    if model != '' or model_number !='':
-        try:
-            with ls.Session(ls.engine) as session:
-                q = session.query(ls.Vagon.model, ls.Vagon.model_number, ls.Vagon.name, ls.Vagon.__table__.c['Тележка'], 
-                                    ls.Vagon.__table__.c['Грузоподъёмность'], ls.Vagon.__table__.c['Объём'],
-                                    ls.Vagon.__table__.c['Год начала серийного производства'], ls.Vagon.__table__.c['Год окончания серийного производства'],
-                                    )
-                if model != '' and model_number !='':
-                    q = q.filter(ls.Vagon.model == model, ls.Vagon.model_number==model_number)
-                elif model == '' and model_number !='':
-                    q = q.filter(ls.Vagon.model_number==model_number)
-                elif model != '' and model_number =='':
-                    q = q.filter(ls.Vagon.model == model)
 
-                q = q.order_by(ls.Vagon.model, ls.Vagon.model_number).all()
-                df = pd.DataFrame(q)
+    try:
+        with ls.Session(ls.engine) as session:
+            q = session.query(ls.Vagon.model, ls.Vagon.model_number, ls.Vagon.name, ls.Vagon.__table__.c['Тележка'], 
+                                ls.Vagon.__table__.c['Грузоподъёмность'], ls.Vagon.__table__.c['Объём'],
+                                ls.Vagon.__table__.c['Год начала серийного производства'], ls.Vagon.__table__.c['Год окончания серийного производства'],
+                                )
+            q = q.filter(ls.Vagon.model.like(f'%{model}%'), ls.Vagon.model_number.like(f"%{model_number}%"))
+            q = q.order_by(ls.Vagon.model, ls.Vagon.model_number).all()
+            df = pd.DataFrame(q)
 
-            df['Модель'] = df.apply(lambda x: x['name'] + ' мод. ' + x['model'] if x['model_number'] is None else x['name'] + ' мод. ' + x['model'] + ' ' + str(int(x['model_number'])), axis=1)
-            df['Грузоподъёмность'] = df.apply(lambda x: None if pd.isnull(x['Грузоподъёмность']) else float(x['Грузоподъёмность'].split()[0]), axis=1)
-            df['Объём'] = df.apply(lambda x: None if pd.isnull(x['Объём']) else float(x['Объём'].split()[0]), axis=1)
-            df.insert(loc=0, column='Характеристики', value=False)
+        df['Модель'] = df.apply(lambda x: x['name'] + ' мод. ' + x['model'] if x['model_number'] is None or np.isnan(x['model_number']) else x['name'] + ' мод. ' + x['model'] + ' ' + str(int(x['model_number'])), axis=1)
+        df['Грузоподъёмность'] = df.apply(lambda x: None if pd.isnull(x['Грузоподъёмность']) else float(x['Грузоподъёмность'].split()[0]), axis=1)
+        df['Объём'] = df.apply(lambda x: None if pd.isnull(x['Объём']) else float(x['Объём'].split()[0]), axis=1)
+        df.insert(loc=0, column='Характеристики', value=False)
+        if carrying_null:
+            df = df[(df['Грузоподъёмность']>=carrying[0]) & (df['Грузоподъёмность']<=carrying[1]) | df['Грузоподъёмность'].isnull()]
+        else:
+            df = df[(df['Грузоподъёмность']>=carrying[0]) & (df['Грузоподъёмность']<=carrying[1])]
         
-            df_button = st.data_editor(df, use_container_width=True, hide_index=True,
-                    column_config = {
-                        'Год начала серийного производства': st.column_config.NumberColumn(label='Начало выпуска', format='%d', width='medium'),
-                        'Год окончания серийного производства': st.column_config.NumberColumn(label='Окончание выпуска', format='%d', width='medium'),
-                        'Грузоподъёмность': st.column_config.NumberColumn(label='Грузоподъёмность, т', format='%.1f'),
-                        'Объём': st.column_config.NumberColumn(label='Объём, м^3', format='%.1f'),
-                    },
-                    disabled=['Модель', 'Тележка','Грузоподъёмность',  'Объём', 'Год начала серийного производства', 'Год окончания серийного производства'],
-                    column_order=('Характеристики', 'Модель', 'Тележка','Грузоподъёмность',  'Объём', 'Год начала серийного производства', 'Год окончания серийного производства'),
-                    )
-        except:
-            st.write('**Вагоны не найдены**')
-    st.caption('Поиск работает только по полному заголовку (можно добавить частичный)')
+        df_button = st.data_editor(df, use_container_width=True, hide_index=True,
+                column_config = {
+                    'Год начала серийного производства': st.column_config.NumberColumn(label='Начало выпуска', format='%d', width='medium'),
+                    'Год окончания серийного производства': st.column_config.NumberColumn(label='Окончание выпуска', format='%d', width='medium'),
+                    'Грузоподъёмность': st.column_config.NumberColumn(label='Грузоподъёмность, т', format='%.1f'),
+                    'Объём': st.column_config.NumberColumn(label='Объём, м^3', format='%.1f'),
+                },
+                disabled=['Модель', 'Тележка','Грузоподъёмность',  'Объём', 'Год начала серийного производства', 'Год окончания серийного производства'],
+                column_order=('Характеристики', 'Модель', 'Тележка','Грузоподъёмность',  'Объём', 'Год начала серийного производства', 'Год окончания серийного производства'),
+                )
+    except:
+        st.write('**Вагоны не найдены**')
+    st.caption('Поиск работает только по полному и частичному заголовку (%mask%)')
     st.caption('Можно добавить фильтры на грузоподьемность, объем и года')
 
 
--- my_lib/lib_sql.py
+++ my_lib/lib_sql.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, func
 from sqlalchemy.orm import declarative_base, Session
 import pandas as pd
 
