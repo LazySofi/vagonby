f6cc4f3dc98de511696120d114a35304e506bdd8
s0fia_p <sofia.m.pisareva@gmail.com>
2023-07-19 10:09:46 +0300

    MetaData

--- my_lib/lib_sql.py
+++ my_lib/lib_sql.py
@@ -15,20 +15,17 @@ class Vagon(Base):
     name =  __table__.c['Наименование']
     gr_vagon = __table__.c['Группа вагонов']
 
-def get_column_headers(engine, table_name = 'vagon'):
-    meta_data = MetaData(bind=engine)
-    MetaData.reflect(meta_data)
+    def get_column_headers(self, table_name = 'vagon'):
+        # Получение объекта таблицы по имени
+        table = Table(table_name, Base.metadata, autoload=True)
 
-    # Получение объекта таблицы по имени
-    table = Table(table_name, meta_data, autoload=True)
+        # Получение заголовков столбцов
+        column_headers = table.columns.keys()
+        dr = ['index', 'Модель', 'Номер модели', 'Наименование', 'Наименование', 'Группа вагонов']
+        column_headers = [i for i in column_headers if i not in dr]
+        column_headers = ['id', 'model', 'model_number', 'name', 'gr_vagon'] + column_headers
 
-    # Получение заголовков столбцов
-    column_headers = table.columns.keys()
-    dr = ['index', 'Модель', 'Номер модели', 'Наименование', 'Наименование', 'Группа вагонов']
-    column_headers = [i for i in column_headers if i not in dr]
-    column_headers = ['id', 'model', 'model_number', 'name', 'gr_vagon'] + column_headers
-
-    return column_headers
+        return column_headers
         
 
 if __name__ == "__main__":
@@ -36,4 +33,4 @@ if __name__ == "__main__":
             a = session.query(Vagon.id, Vagon.model, Vagon.name).all()
             df = pd.DataFrame(a)
     print(df) """
-    print(get_column_headers(engine))
+    print(Vagon.get_column_headers())
